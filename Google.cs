43673fbb2959ee3765e20f479431ae55c0d7bd1c
Tor12378 <87545113+Tor12378@users.noreply.github.com>
2022-10-29 13:45:27 +0300

    2 Задание

---
+++ Google.cs
@@ -0,0 +1,55 @@
+﻿using Docker.DotNet.Models;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools.V104.Cast;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+namespace TestWorkT
+{
+    internal class Program
+    {
+        static void Log(string message)
+        {
+            File.AppendAllText("logPars.txt", message + "\n");
+        }
+        static  void Pars()
+        {
+            Log("Запуск программы" + DateTime.Now);
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = @"https://www.google.com/";
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    driver.FindElement(By.Name("q"));
+                    break;
+                }
+                catch(Exception)
+                {
+                    Thread.Sleep(2000);
+                }
+            }
+            driver.FindElement(By.Name("q")).SendKeys("Hello World");
+            Thread.Sleep(2000);
+            IWebElement ele1 = driver.FindElement(By.Name("btnK"));
+            ele1.Click();
+            string title = driver.Title;
+            Console.WriteLine(title);
+            Log(title);
+            Thread.Sleep(1000);
+            driver.Close();
+            Log("Завершение программы" + DateTime.Now);
+        }
+
+
+        static  void Main(string[] args)
+        {
+            Pars();
+        }
+    }
+}
